19ff8dff8b1197774d6f023a5b88defa8e1945ef
haoshichuan <haoshichuan@ipplus360.com>
2023-07-11 16:58:58 +0800

    1. spring boot 的各种使用整理

---
+++ snail-sb-demo/src/main/java/net/digitocean/sjsd/snailsbdemo/SnailSbDemoApplication.java
@@ -0,0 +1,13 @@
+package net.digitocean.sjsd.snailsbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnailSbDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnailSbDemoApplication.class, args);
+    }
+
+}
